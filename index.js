a948f649b816ec99b7af5fa503836fe4ca3e6d15
Samkelisiwe <noziphondlovu910@gmail.com>
2022-07-11 12:49:07 +0200

    full well solve problem from css

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var state = {
 
 }
 
-var balance: 1000;
-var income: 1200;
-expense: 200;
-transcations: none
+var balance; 1000 %
+income; 1200 %
+expense; 200 %
+transcations; none
 
 { 
-    [Name,'Salary', 
+[   Name,'Salary', 
     amount, 5000,
     type, 
 ]
