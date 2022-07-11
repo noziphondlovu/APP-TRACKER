823d661fa30d34697d6b8fa2dfb7fca211892b8a
Samkelisiwe <noziphondlovu910@gmail.com>
2022-07-11 12:42:45 +0200

    fix css

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var state = {
 var balance: 1000,
 var income: 1200,
 expense: 200,
-transcations: [
-    {  name:'Salary', amount: 5000, type: }
-]
+transcations:
+
+{ 
+    [ name:'Salary', amount: 5000, type:]
+}
+
