cddf60ea496b0d1d3209e7dbc2f1cc5fa607acab
Samkelisiwe <noziphondlovu910@gmail.com>
2022-07-12 08:37:09 +0200

    added something js

--- index.js
+++ index.js
@@ -16,7 +16,32 @@ transcations; none
 
 }
 
+const input = document.querySelector("#add");
+const  between = document.querySelector("#btn");
+const list = document.querySelector("#list");
+var element = document.getElementsByTagName('li');
 
+
+btn.onclick = function(){
+    
+    var text = input.value;
+    if(text ==''){
+        alert('you write something');
+    }else{
+        li = document.createElement('li');
+    li.innerHTML = text;
+    list.insertBefore(li,list.childNodes[0]);
+    input.value = ''
+    }
+    
+}
+
+
+list.onclick = function(alert){
+    if(ev.target.tagName == 'LI'){
+         ev.target.classList.toggle('checked');
+    }
+}
 
 
 
